16f602c03b802ef81ac1d9c61c71786f0da358ff
Frederic PINSARD <fred.pins96@gmail.com>
2023-03-27 16:20:06 +0200

    fix: ex1
    correction classes Calcul et Entreprise
    
    Author: Frederic

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,26 @@
+package ex1;
+
+/** Classe proposant un certain nombre d'operations
+ *
+ * @author Pinsard
+ */
+public class Calcul {
+
+	/** retourn l'addition de deux entiers
+	 * @param a
+	 * @param b
+	 * @return int
+	 */
+	public int addition(int a, int b){
+		return a+b;
+	}
+
+	/** retourne la soustraction de deux entiers
+	 * @param a
+	 * @param b
+	 * @return int
+	 */
+	public int soustraction(int a, int b){
+		return a-b;
+	}
+}
